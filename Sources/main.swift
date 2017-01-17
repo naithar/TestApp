286ac8e6614da3e632954aa6f7bfe9b22db87b21
Naithar <devias.naith@gmail.com>
2017-01-17 11:02:57 +0300

    double configuration

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,56 +1,105 @@
-import CGLFW
-import GLFW
+#if os(iOS)
+import UIKit
 
-import COpenGLOSX
-import SwiftOpenGL
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    class SwiftOpenGLViewController: UIViewController {
+        
+        
+    }
+    
+    public typealias Options = [UIApplicationLaunchOptionsKey : Any]
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: Options? = nil) -> Bool {
+        let frame = UIScreen.main.bounds
+        let window = UIWindow(frame: frame)
+        let viewController = SwiftOpenGLViewController()
+        
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+    }
+}
 
 func main() {
-    glfw.initialize()
-    defer { glfw.terminate() }
-    
-    guard let window1 = glfw.Window(width: 600, height: 600, title: "second") else {
-        return
+    _ = autoreleasepool {
+        return UIApplicationMain(0, nil, nil, NSStringFromClass(AppDelegate.self))
     }
-    
-    window1.callbacks.closed = { window in
-        print("sas")
-    }
-    
-    window1.callbacks.closed = { window in
-        print("close")
-    }
-    
-    
-    
-	while !window1.shouldClose {
-        glfw.pollEvents()
-        
-        window1.makeCurrent()
-        
-        let size = window1.framebuffer.size
-        gl.viewport(x: 0, y: 0, width: size.width, height: size.height)
-        gl.clear(color: gl.Color(red: 0.2, green: 0.4, blue: 0.3, alpha: 1))
-        gl.clear([.buffer, .depth])
-        
-        gl.matrixMode(.projection)
-        gl.loadIdentity()
-        gl.ortho(left: -1, right: 10, bottom: 10, top: -1, near: 0, far: 1)
-        
-        gl.begin(.triangles) {
-            glColor3f(0.1, 0.2, 0.3);
-            var fi: [GLfloat] = [0, 0, 0]
-            glVertex3fv(&fi)
-            var se: [GLfloat] = [2, 0, 0]
-            glVertex3fv(&se)
-            var th: [GLfloat] = [0, 2, 0]
-            glVertex3fv(&th)
-        }
-        
-//        http://www.glfw.org/docs/latest/window_guide.html#window_attribs
-        
-        window1.swapBuffers()
-	}
 }
 
 main()
 
+/*
+     Application - MainWindow. (display) - Context (makeCurrent)
+     Window. (display) - Context (makeCurrent)
+     Window. - Callbacks
+     Notifications
+     
+     Application - Callbacks (mouse, keyboard)
+     Notifications
+     
+     OpenGL
+*/
+#else
+    import CGLFW
+    import GLFW
+    
+    import COpenGLOSX
+    import SwiftOpenGL
+    
+    func main() {
+        glfw.initialize()
+        defer { glfw.terminate() }
+        
+        guard let window1 = glfw.Window(width: 600, height: 600, title: "second") else {
+            return
+        }
+        
+        window1.callbacks.closed = { window in
+            print("sas")
+        }
+        
+        window1.callbacks.closed = { window in
+            print("close")
+        }
+        
+        
+        while !window1.shouldClose {
+            glfw.pollEvents()
+            
+            window1.makeCurrent()
+            
+            let size = window1.framebuffer.size
+            gl.viewport(x: 0, y: 0, width: size.width, height: size.height)
+            gl.clear(color: gl.Color(red: 0.2, green: 0.4, blue: 0.3, alpha: 1))
+            gl.clear([.buffer, .depth])
+            
+            gl.matrixMode(.projection)
+            gl.loadIdentity()
+            
+            gl.ortho(left: -1, right: 10, bottom: 10, top: -1, near: 0, far: 1)
+            
+            gl.begin(.triangles) {
+                glColor3f(0.1, 0.2, 0.3);
+                var fi: [GLfloat] = [0, 0, 0]
+                glVertex3fv(&fi)
+                var se: [GLfloat] = [2, 0, 0]
+                glVertex3fv(&se)
+                var th: [GLfloat] = [0, 2, 0]
+                glVertex3fv(&th)
+            }
+            
+            //        http://www.glfw.org/docs/latest/window_guide.html#window_attribs
+            
+            window1.swapBuffers()
+        }
+    }
+    
+main()
+    
+#endif
