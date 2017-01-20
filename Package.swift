677e017ae6a83c499d057e0cd82f57e9bd48e1a9
Naithar <devias.naith@gmail.com>
2017-01-20 22:10:53 +0300

    package update

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
 import PackageDescription
 
+var dependencies = [Package.Dependency]()
+
+#if os(Linux) || os(macOS)
+dependencies.append(.Package(url: "https://github.com/naithar/GLFW.git", majorVersion: 0))
+dependencies.append(.Package(url: "https://github.com/naithar/OpenGL.git", majorVersion: 0))
+#elseif os(iOS)
+dependencies.append(.Package(url: "https://github.com/naithar/OpenGL.git", majorVersion: 0))
+#endif
+
 let package = Package(
     name: "App", 
-	dependencies: [
-		.Package(url: "https://github.com/naithar/GLFW.git", majorVersion: 0),
-		.Package(url: "https://github.com/naithar/OpenGL.git", majorVersion: 0)
-    ]
+	dependencies: dependencies
 )
