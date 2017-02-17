20b7c95577fd09b3d17b3e80cf4f4bda330aa7ce
Naithar <devias.naith@gmail.com>
2017-02-17 17:02:20 +0300

    upd dep.

--- Package.swift
+++ Package.swift
@@ -3,16 +3,17 @@ import PackageDescription
 var dependencies: [Package.Dependency] = []
 
 dependencies.append(.Package(url: "https://github.com/swift-gl/SGLCore.git", majorVersion: 0))
+dependencies.append(.Package(url: "https://github.com/swift-gl/SOGL.git", majorVersion: 0))
 
 #if os(Linux) || os(macOS)
 dependencies.append(.Package(url: "https://github.com/swift-gl/SGLFW.git", majorVersion: 0))
-dependencies.append(.Package(url: "https://github.com/swift-gl/SOGL.git", majorVersion: 0))
-#elseif os(iOS)
-dependencies.append(.Package(url: "https://github.com/swift-gl/SGLiOS.git", majorVersion: 0))
-dependencies.append(.Package(url: "https://github.com/swift-gl/SOGL.git", majorVersion: 0))
 #endif
 
+// #if os(iOS) || os(macOS)
+// dependencies.append(.Package(url: "https://github.com/swift-gl/SGLiOS.git", majorVersion: 0))
+// #endif
+
 let package = Package(
-    name: "App", 
+    name: "App",
 	dependencies: dependencies
 )
